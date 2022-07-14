1d644459e0b76c3542089526871c3b6839c4f229
Fauzi Achmad Ramadhan <85028034+fauzi417@users.noreply.github.com>
2022-07-14 11:52:32 +0700

    Update autoarima.py

--- autoarima.py
+++ autoarima.py
@@ -1,18 +1,26 @@
 
 #AUTOMATION
 import pmdarima as pm
+from sklearn.model_selection import train_test_split
 
-model3 = pm.auto_arima(df3,
-                      seasonal=True, m=7,
-                      d=1, D=1,
+train,test=train_test_split(df,test_size=.2,shuffle=False)
+model = pm.auto_arima(train,
+                      test='adf',
+                      d=1,
                       start_p=1, start_q=1,
                       max_p=1, max_q=1,
+                      seasonal=True, m=7,
+                      D=1,
+                      start_P=0, start_Q=0
                       max_P=1, max_Q=1,
+                      information_criterion='aic',
                       trace=True,
-                      error_action='ignore',
-                      suppress_warnings=True)
+                      error_action='warn',
+                      suppress_warnings=True,
+                      stepwise = True,
+                      n_job=-1)
 
-print(model3.summary())
+print(model.summary())
 
 import joblib
 filename = 'model_arima.pkl'
